3ae365cf7b1afe75655d859f4edad0c2aec3f57d
Nidhi Singh <107105560+nidsingh10@users.noreply.github.com>
2023-06-08 13:42:25 +0530

    Add files via upload

---
+++ reactsignupform/src/components/SIgn_img.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SIgn_img = () => {
+    return (
+        <>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="sign_img mt-5">
+                    <img src="https://www.stlouisfed.org/-/media/project/frbstl/stlouisfed/blog/2020/january/tradeoffs_opportunitycost_ovgraphic.png" style={{ maxWidth: 400 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SIgn_img
